9b56834ea6aa6c41e562050136fbdb3056cec405
toburau <toburau@gmail.com>
2025-09-09 19:29:03 +0900

    shoot bullet

--- bullet.gd
+++ bullet.gd
@@ -5,12 +5,10 @@ var direction: Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("b1 ", position, global_position)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("b ", position, global_position)
 	position += direction * speed * delta
 	# 画面外に出たら削除
 	if not get_viewport_rect().has_point(global_position):
--- player.gd
+++ player.gd
@@ -6,6 +6,11 @@ var screen_size # Size of the game window.
 @export var bullet: PackedScene
 @export var bullet_speed: float = 600.0
 
+@onready var muzzle = $Muzzle
+
+@export var fire_rate: float = 0.2  # 連射間隔（秒）
+var fire_cooldown: float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -28,7 +33,6 @@ func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if input_vector.length() > 0:
 		velocity = input_vector.normalized() * speed
-		rotation = velocity.angle()
 	else:
 		velocity = Vector2.ZERO
 
@@ -41,26 +45,26 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
-	if velocity.length() > 0:
-		rotation = velocity.angle() + PI/2
-
-	# 弾
+	# 弾	
 	var stick_right_input = Vector2(
 		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
 		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
 	)
-	if stick_right_input.length() < 0.2:
-		stick_right_input = Vector2.ZERO
-	var shoot_dir = stick_right_input.normalized()
-
-	if Input.is_action_just_pressed("shoot"):
-		global_position.x = 200
-		global_position.y = 200
+	var shoot_dir = Vector2.ZERO
+	if stick_right_input.length() > 0.2:
+		shoot_dir = stick_right_input.normalized()
+		rotation = shoot_dir.angle() + PI/2
+	# クールダウンを減らす
+	if fire_cooldown > 0.0:
+		fire_cooldown -= delta
+	if shoot_dir != Vector2.ZERO and fire_cooldown <= 0.0:
 		shoot(shoot_dir)
+		fire_cooldown = fire_rate
 
 func shoot(direction: Vector2) -> void:
 	var newBullet = bullet.instantiate()
-	newBullet.global_position = global_position
+	newBullet.global_position = muzzle.global_position
 	newBullet.direction = direction  # 弾側に `direction: Vector2` を用意
+	newBullet.rotation = direction.angle() + PI/2
 	newBullet.speed = bullet_speed
 	get_tree().current_scene.add_child(newBullet)
