0d9041f4a349d2d39b8845db0d35918c99042475
toburau <toburau@gmail.com>
2025-09-12 20:56:17 +0900

    change player Area2D to CharacterBody2D

--- player.gd
+++ player.gd
@@ -1,7 +1,11 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var player_size := Vector2.ZERO
+
+@export var accel := 600.0   # 加速力
+@export var friction := 300.0 # 摩擦（慣性をどれくらい残すか）
+@export var max_speed := 300.0
 
 @export var bullet: PackedScene
 @export var bullet_speed: float = 600.0
@@ -14,9 +18,11 @@ var fire_cooldown: float = 0.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	var sprite = get_node("Ship") as Sprite2D
+	player_size = sprite.texture.get_size()	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# キーボード入力（8方向）
 	var key_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	# ジョイスティックの左スティック入力（0番コントローラー）
@@ -30,21 +36,16 @@ func _process(delta: float) -> void:
 	# 優先度：スティック > キーボード
 	var input_vector = stick_left_input if stick_left_input.length() > 0 else key_input
 
-	var velocity = Vector2.ZERO
+	# 加速、減速
 	if input_vector.length() > 0:
-		velocity = input_vector.normalized() * speed
+		input_vector = input_vector.normalized()
+		velocity += input_vector * accel * delta
 	else:
-		velocity = Vector2.ZERO
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	# 最大速度
+	if velocity.length() > max_speed:
+		velocity = velocity.normalized() * max_speed
 		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
 	# 弾	
 	var stick_right_input = Vector2(
 		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
@@ -60,6 +61,20 @@ func _process(delta: float) -> void:
 	if shoot_dir != Vector2.ZERO and fire_cooldown <= 0.0:
 		shoot(shoot_dir)
 		fire_cooldown = fire_rate
+		
+	move_and_slide()
+	
+	# X座標をラップ
+	if position.x < -player_size.x/2:
+		position.x = screen_size.x + player_size.x/2
+	elif position.x > screen_size.x + player_size.x/2:
+		position.x = -player_size.x/2
+
+	# Y座標をラップ
+	if position.y < -player_size.y/2:
+		position.y = screen_size.y + player_size.y/2
+	elif position.y > screen_size.y + player_size.y/2:
+		position.y = -player_size.y/2
 
 func shoot(direction: Vector2) -> void:
 	var newBullet = bullet.instantiate()
