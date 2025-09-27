bea08fed3c22073fd7473a5fb18f7f373bb3216b
toburau <toburau@gmail.com>
2025-09-27 21:51:49 +0900

    select title menu

---
+++ title_screen.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/ButtonStart.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://stage.tscn")
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
